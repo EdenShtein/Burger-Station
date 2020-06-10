46d527dda3360b8fe8eac52d5e9b6ddcc068eb8c
Omer Berger <oberger96@cs.colman.ac.il>
2020-06-10 15:46:43 +0300

    Add omer.cs

---
+++ Burger Station/Models/Omer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Burger_Station.Models
+{
+    public class Omer
+    {
+        // Hello World
+    }
+}
