584fdae36f6e8ff9f5dfa06a1dda5a0863cc46f7
Eden Shtein <edenshtein30@cs.colman.ac.il>
2020-06-13 17:18:04 +0300

    Made a new model

--- Burger Station/Models/Item.cs
+++ Burger Station/Models/Item.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Burger_Station.Models
 {
-    public class MenuItem
+    public class Item
     {
         public int Id { get; set; }
         public String Name { get; set; }
         public double Price { get; set; }
+        
+        public ICollection<Comment> comments { get; set; }
+        
+        public ICollection<ItemOrder> itemsOrders { get; set; }
+
     }
 }
