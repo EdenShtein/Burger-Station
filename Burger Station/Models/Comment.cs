4ff8e77538e20e5db95604f6544d95cc68239d29
Omer Berger <oberger96@cs.colman.ac.il>
2020-06-13 17:33:54 +0300

    Add Comment class

---
+++ Burger Station/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Burger_Station.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public User User { get; set; }
+
+        public Item Item { get; set; }
+
+        public DateTime DatePost { get; set; }
+
+        public string TitlePost { get; set; }
+
+        public string BodyPost { get; set; }
+    
+        public int Score { get; set; }
+    }
+}
