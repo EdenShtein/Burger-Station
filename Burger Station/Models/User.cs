8b94725f2dc9b60ee73518f41b55eabb3d77f819
אור לוי <levior9@gmail.com>
2020-06-13 17:39:01 +0300

    Added User & Order

--- Burger Station/Models/User.cs
+++ Burger Station/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,6 +22,11 @@ namespace Burger_Station.Models
 
         //public enum MembershipType { get; set; }
 
+        public Branch favBranch { get; set; }
+
+        public ICollection<Order> listOrders { get; set; }
+
+        public ICollection<Comment> listComments { get; set; }
 
 
 
