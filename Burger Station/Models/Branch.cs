6090d6d37a49022409c0e3692722fa6b4b7916b0
Eden Shtein <edenshtein30@cs.colman.ac.il>
2020-06-13 17:21:25 +0300

    Add branch model

---
+++ Burger Station/Models/Branch.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Burger_Station.Models
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public String phoneNumber { get; set; }
+        public ICollection<User> favUser { get; set; }
+    }
+}
