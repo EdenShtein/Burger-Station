e3e05b09fcc5ef0af437908f7d166562a43fde53
אור לוי <levior9@gmail.com>
2020-06-23 21:20:46 +0300

    map title and variable name changes

--- Burger Station/Models/ItemOrder.cs
+++ Burger Station/Models/ItemOrder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,14 +9,17 @@ namespace Burger_Station.Models
 {
     public class ItemOrder
     {
+        [DisplayName("Item")]
         public int ItemId { get; set; }
 
         public Item Item { get; set; }
 
+        [DisplayName("Order")]
         public int OrderId { get; set; }
 
         public Order Order { get; set; }
 
+        [DisplayName("Branch")]
         public int BranchId { get; set; }
 
         public Branch Branch { get; set; }
